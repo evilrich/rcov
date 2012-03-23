175f4a876c208565d396ca3ed4a06d245ab435bf
Rich Drummond <evilrich@rcdrummond.net>
2012-03-23 16:11:52 -0400

    Added sub-release number for this fork.

--- lib/rcov/version.rb
+++ lib/rcov/version.rb
@@ -3,8 +3,8 @@
 # See LICENSE for licensing information.
 
 module Rcov
-  VERSION = "0.9.11"
-  RELEASE_DATE = "2010-02-28"
+  VERSION = "0.9.11.1"
+  RELEASE_DATE = "2012-03-23"
   RCOVRT_ABI = [2,0,0]
-  UPSTREAM_URL = "http://github.com/relevance/rcov"
+  UPSTREAM_URL = "http://github.com/evilrich/rcov"
 end
